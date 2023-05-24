16589917e79e9eb6bcd39011533b15648aa81a5a
samitkapoor <samitkapoor77@gmail.com>
2023-05-25 03:00:54 +0530

    Fix checkwinner

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,39 @@ export default function App() {
     });
 
     setTurn(turn + 1);
+
+    const winner = checkWinner();
+    if (winner === null) console.log(winner);
+  }
+
+  function checkWinner() {
+    var winCases = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    var currState = gameState[turn];
+    console.log("game states:");
+    console.log(gameState);
+    var winner = null;
+
+    winCases.forEach((winCase) => {
+      if (
+        currState[winCase[0]] === currState[winCase[1]] &&
+        currState[winCase[1]] == currState[winCase[2]]
+      ) {
+        winner = currState[winCase[0]];
+        return winner;
+      }
+    });
+
+    return winner;
   }
 
   return (
